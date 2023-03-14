276db4a6958e533180c9c3541e10dd92f6b97d2f
EL QESSOUAR TARIQ <tariqelqessouar@gmail.com>
2023-03-14 12:11:30 +0100

    refactor: chang tariq by sm in horizontalMiniCard

--- components/horizontalMiniCard.jsx
+++ components/horizontalMiniCard.jsx
@@ -2,7 +2,7 @@ import { CustomImage } from "./customImage"
 
 export function HorizontalMiniCard ({src = "/Images/accommodation/accomodation1.svg", content = "Logement"}) {
     return (
-        <div className="flex h-20 w-96 items-center rounded-lg bg-white drop-shadow-tariq">
+        <div className="flex h-20 w-96 items-center rounded-lg bg-white drop-shadow-sm">
             <div className="w-32">
                 <CustomImage src={src} className="object-cover h-20" />
             </div>
