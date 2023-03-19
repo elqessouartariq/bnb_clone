1a08708899888742e288da2e447e67e208eab5ae
EL QESSOUAR TARIQ <tariqelqessouar@gmail.com>
2023-03-19 20:58:12 +0000

    feat: add threeColumnCard Compomonent

---
+++ components/threeColumnCard.jsx
@@ -0,0 +1,28 @@
+import { TagMini } from "./tagMini"
+import { CustomImage } from "./customImage"
+
+ThreeColumnCard.defaultProps = {
+	price: 577,
+	days: 3,
+	rating: 4.87
+}
+
+export function ThreeColumnCard ({ price, days ,rating }) {
+	return (
+		<div className="w-[290px]  space-y-4 ">
+			<CustomImage src="/Images/wholeWord/Toledo.svg" />            
+			<div className="flex justify-between ">
+				<div className="flex items-center ">
+					<TagMini/>
+					<h3 className="ml-2 first-line:text-secondary-grey   text-shadow-md font-medium ">City name</h3>
+				</div>
+				<div className="flex items-center">
+					<img  src="/icons/starRed.svg" className="h-[10px] w-[10px] mr-1"/>
+					<h3 className="font-medium text-xs first-line text-secondary-dark-grey">		
+						{rating.toFixed(1)}</h3>
+				</div>
+			</div>
+			<h3 className=" text-sm font-normal text-secondary-dark-grey leading-5">À partir de {price}€/personne - {days} jours</h3>
+		</div>
+	)
+}
