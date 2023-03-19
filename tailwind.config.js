72e6911c990b3f45155158b620c5bb5f3b2fd835
EL QESSOUAR TARIQ <tariqelqessouar@gmail.com>
2023-03-19 20:55:12 +0000

    feat: added text-shadow dependencie + extend md

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,13 @@ module.exports = {
 			},
 			dropShadow: {
 				'sm': '0px 0px 15px rgba(0, 0, 0, 0.15)',
-			}
-
+			},
+			textShadow: {
+				'md': '0px 4px 4px rgba(0, 0, 0, 0.25);',
+			 }
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-textshadow')
+	]
 }
