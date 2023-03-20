40bfb1690f5d6566627810b78f1f80c64c7dda09
EL QESSOUAR TARIQ <tariqelqessouar@gmail.com>
2023-03-20 12:13:01 +0000

    refactor:  change Image by adding bnb logo

--- components/forColumnCard.jsx
+++ components/forColumnCard.jsx
@@ -10,7 +10,10 @@ ForColumnCard.defaultProps = {
 export function ForColumnCard ( { price,days,title,src } ) {
 	return (
 		<div className="w-[391px] space-y-3">
-			<CustomImage className="object-cover min-w-[391px]" src={src} />		
+			<div className="relative">
+				<CustomImage className="object-cover min-w-[391px] -z-10" src={src} />
+				<img src="/logoWhite.svg" className="absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  z-10"/>
+			</div>
 			<h3 className="font-bold  text-primary-purple text-xs uppercase">{title}</h3>
 			<h3 className=" text-sm font-normal text-secondary-dark-grey leading-5">À partir de {price}€/personne - {days} jours</h3>
 		</div>
