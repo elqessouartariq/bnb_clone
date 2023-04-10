09fa171cbfc59b4594e7cf826659f6a54fe04d60
elqessouartariq <tariqelqessouar@gmail.com>
2023-04-10 21:37:17 +0000

    refactor: added width to tagMini

--- components/tagMini.jsx
+++ components/tagMini.jsx
@@ -1,13 +1,11 @@
 TagMini.defaultProps = {
-    content: "SUPERHOST"
-}
+	content: "SUPERHOST",
+};
 
-export function TagMini ({ content })
-{
-    return (
-        <button className=" border-solid border-2 p-2 font-bold text-secondary-dark-grey border-secondary-dark-grey">
-            <h2>{content}</h2>
-        </button>
-    )
-   
+export function TagMini({ content }) {
+	return (
+		<button className=" border-solid border-2 p-2 font-bold text-secondary-dark-grey border-secondary-dark-grey w-[146px]">
+			<h2>{content}</h2>
+		</button>
+	);
 }
