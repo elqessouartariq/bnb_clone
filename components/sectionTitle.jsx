4b19ed05d29028316c593001f32c3eea647dd67a
EL QESSOUAR TARIQ <73233952+elqessouartariq@users.noreply.github.com>
2023-04-10 22:14:39 +0000

    Delete sectionTitle.jsx

--- components/sectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-SectionTitle.defaultProps = {
-	title: "Hébergements Airbnb Plus",
-};
-
-export function SectionTitle({ title }) {
-	return (
-		<h1 className=" text-secondary-dark-grey font-bold mb-3">{title}</h1>
-	);
-}
