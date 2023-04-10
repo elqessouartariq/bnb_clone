798f6b423a0f7ee4cf91e1b77f1d99d3ae545023
elqessouartariq <tariqelqessouar@gmail.com>
2023-04-10 22:08:32 +0000

    refactor:  Home Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,25 @@
+import FeaturedDestiationSection from "../../components/ui/featuredDestinations/FeaturedDestiationSection";
+import AdventureSection from "../../components/ui/adventures/Adventuresection";
+import ExploreSection from "../../components/ui/explore/ExploreSection";
+import HighlyRatedSection from "../../components/ui/highlyRated/HighlyRatedSection";
+import WholeWorldSection from "../../components/ui/wholeWord/WholeWorldSection";
+import AccommodationSection from "../../components/ui/accommodation/AccommodationSection";
+import { HeroSection } from "../../components/ui/hero/HeroSection";
 
 export default function Home() {
-  return (
-    <main className=" text-primary-purple">
-     <h3>Hello world</h3>
-    </main>
-  )
+	return (
+		<>
+			<header>
+				<HeroSection />
+			</header>
+			<main className="m-20">
+				<ExploreSection />
+				<AccommodationSection />
+				<AdventureSection />
+				<WholeWorldSection />
+				<HighlyRatedSection />
+				<FeaturedDestiationSection />
+			</main>
+		</>
+	);
 }
