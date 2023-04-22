9c1537a875b1fc6ef3f8255132de221309106f45
elqessouartariq <tariqelqessouar@gmail.com>
2023-04-22 02:52:10 +0000

    refactor make nav clickable +add children to Header

--- components/ui/hero/Header.jsx
+++ components/ui/hero/Header.jsx
@@ -1,18 +1,37 @@
-export function Header() {
+export function Header({ children }) {
 	return (
-		<div className="flex justify-between p-3">
-			<div>
-				<img src="/logoWhite.svg" className=" h-9 w-9" />
+		<>
+			<div className="flex justify-between p-3">
+				<div>
+					<a href="#">
+						<img src="/logoWhite.svg" className=" h-9 w-9" />
+					</a>
+				</div>
+				<ul className="flex space-x-10 text-white">
+					<li>
+						<a href="#">Français (FR)</a>
+					</li>
+					<li>
+						<a href="#">EUR €</a>
+					</li>
+					<li>
+						<a href="#">Devenir hôte</a>
+					</li>
+					<li>
+						<a href="#">Créer son expérience</a>
+					</li>
+					<li>
+						<a href="#">Aide</a>
+					</li>
+					<li>
+						<a href="#">Inscription</a>
+					</li>
+					<li>
+						<a href="#">Connexion</a>
+					</li>
+				</ul>
 			</div>
-			<ul className="flex space-x-10 text-white"> 
-				<li>Français (FR)</li>
-				<li>EUR €</li>
-				<li>Devenir hôte</li>
-				<li>Créer son expérience</li>
-				<li>Aide</li>
-				<li>Inscription</li>
-				<li>Connexion</li>
-			</ul>
-		</div>
-	)
+			{children}
+		</>
+	);
 }
